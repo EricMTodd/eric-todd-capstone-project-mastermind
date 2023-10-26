7a817650f19bc44c2ced140e98894d4658c44d8b
EricMTodd <eterictodd@gmail.com>
2023-10-26 17:48:27 -0600

    Minor formatting changes.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # eric-todd-capstone-project-mastermind
 
 Notes:
-- Guesses and feedback will be stored in arrays.
-- Learn how to create objects.
-- Focus on the core gamplay first, then implement scoring and leaderboard features.
+- Create a development branch from the working main branch.
--- mastermind.cpp
+++ mastermind.cpp
@@ -6,17 +6,20 @@
 
 using namespace std;
 
+// Variable declarations
 string codeElements[6] = {"RD", "BU", "YW", "GN", "WH", "BK"};
 string randomCode[4] = {};
-
 int chancesRemaining = 8;
 
+// Function declarations
 void generateRandomCode();
 void getPlayerInput();
 void validatePlayerInput(string playerInput[4]);
 void evaluatePlayerInput(string playerInput[4]);
 
+// Main function
 int main() {
+  // Generate random code to be broken
   generateRandomCode();
   cout << endl;
 
@@ -27,21 +30,14 @@ int main() {
   }
   cout << endl << endl;
 
+  // Defines clues for the player
   cout << "Feedback key: " << endl;
   cout << "[WH] = Right color, right position." << endl;
   cout << "[RD] = Right color, wrong position." << endl;
   cout << "[BK] = Wrong color." << endl;
-
   cout << endl;
 
-  // Prints randomCode
-  // cout << "randomCode: ";
-  // for (int i = 0; i < 4; i++) {
-  //   cout << "[" << randomCode[i] << "]";
-  // }
-  // cout << endl;
-
-  // Prompt player to make a guess
+  // Start core game loop.
   cout << "You have " << chancesRemaining << " attempts to break the code.\n";
   getPlayerInput();
   cout << endl;
@@ -49,6 +45,9 @@ int main() {
   return 0;
 }
 
+// Function definitions
+
+// Generates a random code
 void generateRandomCode() {
   srand((unsigned) time(NULL));
   for (int i = 0; i < 6; i++) {
@@ -57,6 +56,7 @@ void generateRandomCode() {
   };
 };
 
+// Gathers input from the player
 void getPlayerInput() {
   string playerInput[4];
   cout << "Input guess: ";
@@ -64,6 +64,7 @@ void getPlayerInput() {
   validatePlayerInput(playerInput);
 };
 
+// Verifies clean inputs
 void validatePlayerInput(string playerInput[4]) {
   for (int i = 0; i < 4; i++) {
     if (playerInput[i] == codeElements[0] || playerInput[i] == codeElements[1] || playerInput[i] == codeElements[2] || playerInput[i] == codeElements[3] || playerInput[i] == codeElements[4] || playerInput[i] == codeElements[5]) {
@@ -76,6 +77,7 @@ void validatePlayerInput(string playerInput[4]) {
   return evaluatePlayerInput(playerInput);
 }
 
+// Evaluates player guesses and provides appropriate feedback
 void evaluatePlayerInput(string playerInput[4]) {
   // Evaluate win condition
   if (playerInput[0] == randomCode[0] && playerInput[1] == randomCode[1] && playerInput[2] == randomCode[2] && playerInput[3] == randomCode[3]) {
@@ -96,7 +98,6 @@ void evaluatePlayerInput(string playerInput[4]) {
     chancesRemaining--;
     cout << "\n\nChances remaining: " << chancesRemaining << endl;
   }
-
   // Evaluate lose condition
   if (chancesRemaining == 0) {
     cout << "YOU LOSE!\n";
